9a3a307e76b06baf2c44a45c50d498ecb64b371d
Dev Patel <devp@zignuts.com>
2026-04-16 17:02:35 +0530

    Fix ProductSchema in zod

--- zod/productSchema.ts
+++ zod/productSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const productSchema = z.object({
   title: z
     .string()
-    .min(3, "Title must be at least 3 characters"),
+    .min(3, "Title must be at least 3 characters").regex(/^[a-zA-Z\s]+$/, "Name can only contain letters and spaces"),
 
   price: z
     .number("Price must be a number")
@@ -11,11 +11,11 @@ export const productSchema = z.object({
 
   description: z
     .string()
-    .min(10, "Description must be at least 10 characters"),
+    .min(10, "Description must be at least 10 characters").regex(/^[a-zA-Z0-9\s,.-]+$/, "Address can only contain letters, numbers, spaces, commas, periods, and hyphens"),
 
   category: z
     .string()
-    .min(2, "Category is required"),
+    .min(2, "Category is required").regex(/^[a-zA-Z\s]+$/, "Name can only contain letters and spaces"),
 });
 
 export type ProductType = z.infer<typeof productSchema>;
